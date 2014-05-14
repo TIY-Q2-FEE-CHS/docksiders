4c17e7d824023694e71fd64632eb779d8df39f2f
brookebc <brooke.casey@gmail.com>
2014-05-14 15:32:49 -0400

    started my own searching page to work with search js to get the api working without disruption

--- app/scripts/search.js
+++ app/scripts/search.js
@@ -2,28 +2,105 @@
 
 $(document).ready(function() {
 
-  explorerReviews.init();
+  placesData.init();
 
 });
 
-var explorerReviews = {
+var placesData = {
+
   init: function() {
-    this.initStyling();
+    // this.initStyling();
     this.initEvents();
   },
   initStyling: function() {
     
 
   },
-  initEvents: function() {
+  initEvents: function() { 
 
-    
+  	this.locateYou();
+  	
+  },
 
+findPlaces: function (){
+
+	// $.ajax({
+ //      url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=50&types=park&name=park&sensor=false&key=AIzaSyBVsHwZR7Gk2n4EBh0ZAvg2yDbRmMsza3g",
+ //      type: "GET",
+ //      dataType: "json",
+ //      error: function(jqXHR, status, error) {
+ //        alert("Something is Wrong" + error);
+ //      },
+ //      success: function(data, dataType, jqXHR) {
+ //        console.log("success");
+
+ //      }
+ //    });
+},
+
+locateYou: function(){
+	
+                function success(position) {
+                      var s = document.querySelector('#status');
+                      
+                      if (s.className == 'success') {
+                          
+                        return;
+                      }
+                      
+                      s.innerHTML = "found you!";
+                      s.className = 'success';
+                      
+                      var mapcanvas = document.createElement('div');
+                      mapcanvas.id = 'mapcanvas';
+                      mapcanvas.style.height = '400px';
+                      mapcanvas.style.width = '560px';
+                        
+                      document.querySelector('article').appendChild(mapcanvas);
+                      
+                      var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                      var myOptions = {
+                        zoom: 15,
+                        center: latlng,
+                        mapTypeControl: false,
+                        navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                      };
+                      var map = new google.maps.Map(document.getElementById("mapcanvas"), myOptions);
+                      
+                      var marker = new google.maps.Marker({
+                          position: latlng, 
+                          map: map, 
+                          title:"You are here! (at least within a "+position.coords.accuracy+" meter radius)"
+                      });
+                }
+
+                function error(msg) {
+                  var s = document.querySelector('#status');
+                  s.innerHTML = typeof msg == 'string' ? msg : "failed";
+                  s.className = 'fail';
+                  
+                }
+
+                if (navigator.geolocation) {
+                  navigator.geolocation.getCurrentPosition(success, error);
+                } else {
+                  error('not supported');
+                }
+              
+}
+
+}
+
+  	
   }
 
+}
 
-$.ajax({
-      url: " ",
+findPlaces: function (){
+
+	$.ajax({
+      url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=50&types=park&name=park&sensor=false&key=AIzaSyBVsHwZR7Gk2n4EBh0ZAvg2yDbRmMsza3g",
       type: "GET",
       dataType: "json",
       error: function(jqXHR, status, error) {
@@ -34,3 +111,60 @@ $.ajax({
 
       }
     });
+},
+
+
+locateYou: function(){
+	
+                function success(position) {
+                      var s = document.querySelector('#status');
+                      
+                      if (s.className == 'success') {
+                        // not sure why we're hitting this twice in FF, I think it's to do with a cached result coming back    
+                        return;
+                      }
+                      
+                      s.innerHTML = "found you!";
+                      s.className = 'success';
+                      
+                      var mapcanvas = document.createElement('div');
+                      mapcanvas.id = 'mapcanvas';
+                      mapcanvas.style.height = '400px';
+                      mapcanvas.style.width = '560px';
+                        
+                      document.querySelector('article').appendChild(mapcanvas);
+                      
+                      var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                      var myOptions = {
+                        zoom: 15,
+                        center: latlng,
+                        mapTypeControl: false,
+                        navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                      };
+                      var map = new google.maps.Map(document.getElementById("mapcanvas"), myOptions);
+                      
+                      var marker = new google.maps.Marker({
+                          position: latlng, 
+                          map: map, 
+                          title:"You are here! (at least within a "+position.coords.accuracy+" meter radius)"
+                      });
+                }
+
+                function error(msg) {
+                  var s = document.querySelector('#status');
+                  s.innerHTML = typeof msg == 'string' ? msg : "failed";
+                  s.className = 'fail';
+                  
+                }
+
+                if (navigator.geolocation) {
+                  navigator.geolocation.getCurrentPosition(success, error);
+                } else {
+                  error('not supported');
+                }
+              
+},
+
+}
+
