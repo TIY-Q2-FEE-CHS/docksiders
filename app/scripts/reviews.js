2ee158fb2d72a3ed0cb283fd7d8066bb80888078
Katherine Mattison <kbmedisto@gmail.com>
2014-05-14 13:45:44 -0400

    added details html page and edited reviews ajax and init js file

--- app/scripts/reviews.js
+++ app/scripts/reviews.js
@@ -1,6 +1,35 @@
 // Starting review js for CRUD Explorer Reviews
-$.ajax({
-  type: "GET",
-  url: "test.js",
-  dataType: "json"
+$(document).ready(function() {
+
+  explorerReviews.init();
+
 });
+
+var explorerReviews = {
+  init: function() {
+    this.initStyling();
+    this.initEvents();
+  },
+  initStyling: function() {
+    
+
+  },
+  initEvents: function() {
+
+    
+
+  }
+
+
+$.ajax({
+      url: "http://tiy-fee-rest.herokuapp.com/collections/explorerReviews",
+      type: "GET",
+      dataType: "json",
+      error: function(jqXHR, status, error) {
+        alert("Something is Wrong" + error);
+      },
+      success: function(data, dataType, jqXHR) {
+        console.log("success");
+
+      }
+    });
